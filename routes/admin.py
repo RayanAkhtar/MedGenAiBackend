0b8da652f915e204e4a5787aa704596c0064f5bf
Mahdi-01-dev <baizeed.ahmed@outlook.com>
2025-02-12 13:27:04 +0000

    debug: update symbols once again to get match between route and service

--- routes/admin.py
+++ routes/admin.py
@@ -187,11 +187,11 @@ def get_image_ml_metrics(image_id):
     try:
         confusion_matrix = get_image_confusion_matrix(image_id)
 
-        TP = confusion_matrix.get('truePositive', 0)
-        FP = confusion_matrix.get('falsePositive', 0)
-        FN = confusion_matrix.get('falseNegative', 0)
-        TN = confusion_matrix.get('trueNegative', 0)
-
+        TP = confusion_matrix.get('truepositive', 0)
+        FP = confusion_matrix.get('falsepositive', 0)
+        FN = confusion_matrix.get('falsnegative', 0)
+        TN = confusion_matrix.get('truenegative', 0)
+        print("confusion matrix is", confusion_matrix)
         accuracy = (TP + TN) / (TP + FP + FN + TN) if (TP + FP + FN + TN) > 0 else 0
         precision = TP / (TP + FP) if (TP + FP) > 0 else 0
         recall = TP / (TP + FN) if (TP + FN) > 0 else 0
