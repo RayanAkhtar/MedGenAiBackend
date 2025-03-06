95a54f62f264f480a56b8f66bccf3438482f8644
RayanAkhtar <113346804+RayanAkhtar@users.noreply.github.com>
2025-03-06 12:42:48 +0000

    feat: update calculation for backend user page

--- services/admin/users.py
+++ services/admin/users.py
@@ -1,23 +1,25 @@
-from models import db, Users, UserGameSession, UserGuess, Images
+from sqlalchemy import func
+from models import db, Users, UserGameSession, UserGuess, Images, UserTags, Tag
 
 def get_accuracy_for_user(username: str) -> float:
     user = Users.query.filter_by(username=username).first()
     if not user:
         raise ValueError(f"User {username} not found")
 
-    total_guesses = db.session.query(UserGuess).join(UserGameSession).filter(UserGameSession.user_id == user.user_id).count()
+    total_guesses = db.session.query(func.count(func.distinct(UserGuess.guess_id))).join(UserGameSession).filter(
+        UserGameSession.user_id == user.user_id
+    ).scalar()
 
-    correct_guesses = db.session.query(UserGuess).join(UserGameSession).join(Images).filter(
+    correct_guesses = db.session.query(func.count(func.distinct(UserGuess.guess_id))).join(UserGameSession).join(Images).filter(
         UserGameSession.user_id == user.user_id,
         UserGuess.user_guess_type == Images.image_type
-    ).count()
+    ).scalar()
 
     if total_guesses == 0:
         return 0.0
 
     accuracy = (correct_guesses / total_guesses) * 100
-    return accuracy
-
+    return round(accuracy, 2)
 
 
 def get_total_images_attempted_for_user(username: str) -> int:
@@ -25,6 +27,8 @@ def get_total_images_attempted_for_user(username: str) -> int:
     if not user:
         raise ValueError(f"User {username} not found")
 
-    total_images_attempted = db.session.query(UserGuess).join(UserGameSession).filter(UserGameSession.user_id == user.user_id).count()
+    total_images_attempted = db.session.query(func.count(func.distinct(UserGuess.guess_id))).join(UserGameSession).filter(
+        UserGameSession.user_id == user.user_id
+    ).scalar()
     
     return total_images_attempted
