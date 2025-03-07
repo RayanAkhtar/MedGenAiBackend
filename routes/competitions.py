fcbac7a82115edaf1750f922840af1045c14a4db
AAreeb13 <areeb.ahmad67@outlook.com>
2025-03-07 01:20:58 +0000

    feat: add api to fetch game data

--- routes/competitions.py
+++ routes/competitions.py
@@ -1,6 +1,6 @@
 
 from flask import jsonify, send_from_directory, Blueprint, request
-from services.competitions import get_all_competitions, get_competition, submit_competition_score, create_competition
+from services.competitions import get_all_competitions, get_competition, get_game_by_game_id, submit_competition_score, create_competition
 bp = Blueprint('competitions', __name__)
 
 
@@ -29,3 +29,8 @@ def submit():
   json_data = request.get_json()
   print(f"Submitting to competiton with ID: {json_data.competition_id}, for user: {json_data.user_id}")
   return submit_competition_score(json_data.competition_id, json_data.user_id, json_data.score)
+
+
+@bp.route('/api/getGame/<int:game_id>', methods=['GET'])
+def get_game(game_id):
+    return jsonify(get_game_by_game_id(game_id))
--- services/competitions.py
+++ services/competitions.py
@@ -94,3 +94,29 @@ def submit_competition_score(competition_id, user_id, score):
     except Exception as e:
         db.session.rollback()
         return jsonify({'error': str(e)}), 500
+
+def get_game_by_game_id(game_id):
+    try:
+        # Fetch the game from the database
+        game = db.session.query(Game).filter_by(game_id=game_id).first()
+        created_by = db.session.query(Users).filter_by(user_id=game.created_by).first()
+
+        # If game not found, return an error message
+        if not game:
+            return {"error": "Game not found"}
+
+        # Construct the response
+        game_data = {
+            "game_id": game.game_id,
+            "game_mode": game.game_mode,
+            "date_created": game.date_created,
+            "game_board": game.game_board,
+            "game_status": game.game_status,
+            "expiry_date": game.expiry_date,
+            "created_by": created_by.username,
+       }
+
+        return game_data, 200
+    except Exception as e:
+        return {"error": str(e)}, 404
+    
