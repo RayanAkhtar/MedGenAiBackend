f86359c0bce0bd35bc592e5094d8a6a49ae18209
AAreeb13 <areeb.ahmad67@outlook.com>
2025-03-07 02:59:17 +0000

    fix: remove logging

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
 import os
 from __init__ import create_app
-import logging
 app = create_app()
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)  # Set logging level to DEBUG
-    app.logger.setLevel(logging.DEBUG)  # Ensure Flask logs debug messages
-
-    app.logger.info("This is an info log")
-    app.logger.debug("This is a debug log")
-    app.logger.error("This is an error log")
     app.run(debug=True, host="0.0.0.0", port=int(os.getenv("PORT", 5328)))
 
