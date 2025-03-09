0470d3197c5bdf832828d745dbcd9e1e8abf8d4b
Humza <hh1322@ic.ac.uk>
2025-03-09 17:19:10 +0000

    changed name

--- models.py
+++ models.py
@@ -204,6 +204,6 @@ def ensure_date_equality(mapper, connection, target):
     # Fetch the associated game
     game = db.session.query(Game).filter_by(game_id=target.competition_id).first()
     if game:
-        game.expiry_date = target.expiry_date
+        game.expiry_date = target.end_date
     else:
         raise ValueError(f"No game found with ID {target.competition_id}!")
